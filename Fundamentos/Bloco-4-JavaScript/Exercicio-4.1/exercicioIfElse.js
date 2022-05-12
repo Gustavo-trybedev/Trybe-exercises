dc686e1f8836ce2720bdf215dfa3a36ae503cd09
gustavoBarros <gustavo.dutra11@gmail.com>
2022-05-12 16:30:45 -0300

    Exercicios-Dia-4.1

---
+++ Fundamentos/Bloco-4-JavaScript/Exercicio-4.1/exercicioIfElse.js
@@ -0,0 +1,15 @@
+const nota = 65;
+
+if(nota >= 80){
+    console.log("Parabéns, você foi aprovado!");
+} 
+
+else if(nota < 80 && nota >= 60) {
+        console.log("Você está na nosa lista de espera");
+
+}
+
+else {
+        console.log("Você foi reprovado");
+}
+
---
+++ Fundamentos/Bloco-4-JavaScript/Exercicio-4.1/exercicioVariáveis.js
@@ -0,0 +1,15 @@
+const myName = "Gustavo";
+const birthCity = "Rio de Janeiro";
+let birthYear  = 1992;
+
+console.log(myName);
+
+
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "São Paulo";
+console.log(birthCity);
+
+// Houve retorno com mensagem de erro pois tentamos alterar o valor de uma constante, declarada através do "const".
