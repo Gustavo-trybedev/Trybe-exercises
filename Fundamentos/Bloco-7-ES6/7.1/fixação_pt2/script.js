cb5b8c6032319e8938a552a4e46138b9d8fdd502
gustavoBarros <gustavo.dutra11@gmail.com>
2022-06-09 19:15:50 -0300

    Exercício 4 - Parte 2 - de fixação

--- Fundamentos/Bloco-7-ES6/7.1/fixação_pt2/script.js
+++ Fundamentos/Bloco-7-ES6/7.1/fixação_pt2/script.js
@@ -42,3 +42,35 @@ const counter = document.querySelector('#click-counter');
 let clickCount = 0;
 
 button.addEventListener('click', () => { counter.innerHTML = clickCount += 1; });
+
+// Função 1: Escreva uma função que substitua a letra 'x' em uma frase.
+// A função deverá receber um nome por parâmetro;
+// Declare dentro da função uma variável com o nome frase, do tipo const e com o valor igual a 'Tryber x aqui!';
+// A função deverá retornar uma nova frase onde o x da frase 'Tryber x aqui!' seja substituído pelo nome passado por parâmetro.
+// Exemplo:
+// Parâmetro: 'Bebeto'
+// Retorno: 'Tryber Bebeto aqui!'
+
+const replaceX = (name) => {
+    const frase = 'Tryber x aqui!';
+    return frase.replace('x', name);
+}
+console.log(replaceX('Bebeto'));
+
+// Função 2: Escreva uma função que irá receber o retorno da Função 1 por parâmetro e retornará uma nova string.
+// A função deverá receber o retorno da Função 1 por parâmetro;
+// Declare dentro da função uma variável com o nome skills, do tipo const;
+// A variável skills deverá ser um array contendo três strings com tecnologias que você já aprendeu.
+// Concatene o valor retornado da Função 1, a frase 'Minhas três principais habilidades são:' e o valor da variável skills.
+// Exemplo de retorno:
+// Tryber Bebeto aqui!
+// Minhas três principais habilidades são:
+// JavaScript
+// HTML
+// CSS
+
+const concat = (name) => {
+    const skills = ['JavaScript', 'HTML', 'CSS'];
+    return `Tryber ${name} aqui ! Minhas principais habilidades são: ${skills}`;
+}
+console.log(concat('Bebeto'));
