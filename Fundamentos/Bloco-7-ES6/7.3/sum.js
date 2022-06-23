781260e5a44339a583ea922c339ae563950a2e76
gustavoBarros <gustavo.dutra11@gmail.com>
2022-06-23 19:02:19 -0300

    myRemove

--- Fundamentos/Bloco-7-ES6/7.3/sum.js
+++ Fundamentos/Bloco-7-ES6/7.3/sum.js
@@ -1,9 +1,5 @@
 // 🚀 1 - A função sum(a, b) retorna a soma do parâmetro a com o b
 
-
-
-
-
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('parameters must be numbers');
