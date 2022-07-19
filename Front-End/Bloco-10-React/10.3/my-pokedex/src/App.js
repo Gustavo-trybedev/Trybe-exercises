067a4a01ea406a26df490ebc8e5eebccf0353c9c
gustavoBarros <gustavo.dutra11@gmail.com>
2022-07-19 19:38:28 -0300

    Pokedex

---
+++ Front-End/Bloco-10-React/10.3/my-pokedex/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Pokedex from './components/Pokedex';
+import './App.css';
+import pokemons from './data';
+
+class App extends React.Component {
+  render() {
+  return (
+    <div className="App">
+     <h1>Pokedex</h1>
+      <Pokedex pokemons={pokemons} />
+    </div>
+  );
+
+}
+}
+
+export default App;
